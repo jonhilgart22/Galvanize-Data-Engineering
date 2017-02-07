5f3081397829f8fe2968da7851b81145fc8b08c2
jonathanhilgart <jonathan.hilgart@gmail.com>
2017-02-06 18:35:58 -0800

    lab 3.3

--- 4.1 - Message Brokers/lab-3.3-Jonathan-Lukeman-Kinesis.py
+++ 4.1 - Message Brokers/lab-3.3-Jonathan-Lukeman-Kinesis.py
@@ -4,25 +4,33 @@ import os
 import yaml
 import json
 import boto3
-credentials = yaml.load(open(os.path.expanduser('~/.ssh/api_credentials.yml')))
-# authenticate with Twitter
-twitter_stream = TwitterStream(auth=OAuth(**credentials['twitter']))
-# use boto here
-client = boto3.client('firehose', region_name='us-east-1')
-# #get Twitter tweets
-sample = twitter_stream.statuses.sample()
-while True:
-    for tweet in sample:
-        try:
-            tweet['delete']  # this will find deleted tweets
-            # and not insert them into
-            print('Found a deleted tweet')
-        except:
+def insert_twitter_firehouse():
+
+    """Insert twitter tweets into s3 via Amazon kinesis firehose."""
+    credentials = yaml.load(open(os.path.expanduser(
+                '~/.ssh/api_credentials.yml')))
+    # authenticate with Twitter
+    twitter_stream = TwitterStream(auth=OAuth(**credentials['twitter']))
+    # use boto here
+    client = boto3.client('firehose', region_name='us-east-1')
+    # #get Twitter tweets
+    sample = twitter_stream.statuses.sample()
+    while True:
+        for tweet in sample:
             try:
-                client.put_record(
-                            DeliveryStreamName='twitter-streaming-data',
-                            Record={'Data': json.dumps(str(tweet)+'\n')})
-                print('wrote a tweet!')
+                tweet['delete']  # this will find deleted tweets
+                # and not insert them into
+                print('Found a deleted tweet')
             except:
-                pass
-print('Exited the while loop')
+                try:
+                    client.put_record(
+                                DeliveryStreamName='twitter-streaming-data',
+                                Record={'Data': json.dumps(str(tweet)+'\n')})
+                    print('wrote a tweet!')
+                except:
+                    pass
+    print('Exited the while loop')
+
+if __name__ == '__main__':
+
+    insert_twitter_firehouse()
