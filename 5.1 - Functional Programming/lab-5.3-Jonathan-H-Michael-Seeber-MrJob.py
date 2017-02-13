54917c0c07db31aa7c85e8676c07c49be41f4f79
jonathanhilgart <jonathan.hilgart@gmail.com>
2017-02-13 15:34:34 -0800

    lab 5.1

---
+++ 5.1 - Functional Programming/lab-5.3-Jonathan-H-Michael-Seeber-MrJob.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# http://mrjob.readthedocs.io/en/latest/guides/writing-mrjobs.html#multi-step-jobs
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+import json
+from collections import Counter
+from collections import defaultdict
+
+
+class MRMostUsedWord(MRJob):
+
+    def mapper_get_words(self, _, line):
+        """Take in stdin and return each hashtag with the number one"""
+
+        for tweet in line.split('/n'):
+            try:
+                entity = json.loads(tweet)
+                try:
+                    entity = entity['entities']
+                    if entity is not None and (entity['hashtags'] is not None
+                                            or len(entity['hashtags']) != 0):
+                        for hashtags in entity['hashtags']:
+                            yield(hashtags['text'].lower(), 1)
+                    else:
+                        pass
+                except KeyError:
+                    continue
+            except ValueError:
+                continue
+
+    def combiner_count_words(self, word, counts):
+        # sum the words we've seen so far
+        yield (word, sum(counts))
+
+    def reducer_count_words(self, word, counts):
+        # send all (num_occurrences, word) pairs to the same reducer.
+        # num_occurrences is so we can easily use Python's max() function.
+        yield None, (sum(counts), word)
+
+    def reducer_find_top_hashtags(self, _, word_count_pairs):
+        # return the top ten hashtags from the word_count_pairs generator
+        top_hashtags = defaultdict(int)
+        current_hashtag = ''
+        for hashtag_number in list(word_count_pairs):
+            top_hashtags[hashtag_number[1]] += hashtag_number[0]
+        for hash_t, num in Counter(top_hashtags).most_common(20):
+            print("{}\t{}".format(hash_t, num))
+
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper_get_words,
+                   combiner=self.combiner_count_words,
+                   reducer=self.reducer_count_words),
+            MRStep(reducer=self.reducer_find_top_hashtags)
+        ]
+
+
+if __name__ == '__main__':
+    MRMostUsedWord.run()
