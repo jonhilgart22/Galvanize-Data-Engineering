d2a1044340561f69eaeec7080f680974a96d5a46
Jonathan <jonathan.hilgart@gmail.com>
2017-02-28 09:21:27 -0800

    final links

--- Final_Project/weather-bart-analysis-normalization-spark.py
+++ Final_Project/weather-bart-analysis-normalization-spark.py
@@ -1,51 +1,50 @@
-
 # coding: utf-8
-
-# In[ ]:
 
 ## Here are the links to the normalized data
 ## weather
-# 	https://s3.amazonaws.com/normalized-data-weather-bart/location2017/2/26/17/part-00000-e2cbf24e-080b-42c9-816b-abc421d2bc28.csv
-# 	https://s3.amazonaws.com/normalized-data-weather-bart/main-temp2017/2/26/17/part-00000-9ad2cc92-5776-4f21-a4a4-1f0e86cf49fc.csv
-# 	https://s3.amazonaws.com/normalized-data-weather-bart/weather-description2017/2/26/17/part-00000-8b0d083c-f41e-4f00-95f8-1ba26d84633a.csv
-# 	https://s3.amazonaws.com/normalized-data-weather-bart/wind_df2017/2/26/17/part-00000-87d3f008-0fdd-4c3f-ae1c-c8e3dc7061ec.csv
+# 	https://s3.amazonaws.com/normalized-data-weather-bart/location2017/02/27/part-00000-0a9f0438-b65c-4d9e-b8ab-20ef1d0b37d9.csv
+# 		https://s3.amazonaws.com/normalized-data-weather-bart/main-temp2017/02/27/part-00000-48bad1d8-4400-4309-8fbd-a735d4ae998b.csv
+# https://s3.amazonaws.com/normalized-data-weather-bart/weather-description2017/02/27/part-00000-44f6bedd-b0cd-4e41-9590-4ce73eb28fde.csv
+# 		https://s3.amazonaws.com/normalized-data-weather-bart/wind_df2017/02/27/part-00000-b02ec65e-946c-4e25-b5ce-85568f67115a.csv
 ## bart
 # 	https://s3.amazonaws.com/normalized-data-weather-bart/bart-one-arrival2017/2/27/17/part-00000-7b169176-4c09-4fb4-b943-c328a64630d4.csv
 # 	https://s3.amazonaws.com/normalized-data-weather-bart/bart-one-physical2017/2/27/17/part-00000-b3d0331e-0738-4e14-9196-2fc6d69575b3.csv
-
-
-# In[22]:
 
 from pyspark.sql.functions import  explode, from_unixtime, from_json
 from pyspark.sql import SQLContext, column
 from pyspark.sql import DataFrame
 from pyspark.sql import *
 from pyspark.sql.functions import *
+from pyspark.sql.types import DateType
+from pyspark import SparkConf, SparkContext # for running spark-submit job
 import time
 import pytz
 import datetime
-from pyspark.sql.types import DateType
-from pyspark import SparkConf, SparkContext # for running spark-submit job
 
 
-# when running spark-submit, need to create the spark context
+
+# # when running spark-submit, need to create the spark context
 sc = SparkContext()
 spark = SparkSession(sc)
 sqlContext = SQLContext(sc)
 
 # get the time for saving and uploading files
+yesterday = datetime.date.today()-datetime.timedelta(1)
 SF_time = pytz.timezone('US/Pacific')
 current_sf_time = datetime.datetime.now(SF_time)
 date_sf, raw_time_sf = time.strftime('{}'.format(current_sf_time)).split(' ')
 sf_hour, sf_minute = int(raw_time_sf[:2]), int(raw_time_sf[3:5])
-now = datetime.datetime.now()
-sf_year = now.year
-sf_month = now.month
+sf_year = yesterday.year
+sf_month = yesterday.month
+sf_day = yesterday.day   # compute yesterday's files
 if len(str(sf_month)) < 2:
     sf_month = '0'+str(sf_month)
-sf_day = now.day
+if len(str(sf_day)) < 2:
+    sf_day = '0'+str(sf_day)
+
 ## this is for the batch jobs to save the file location
 KeyFileName = "{}/{}/{}".format(sf_year, sf_month, sf_day)
+print(KeyFileName)
 weather_path = "s3a://current-weather-data/{}/{}/{}/*/*".format(sf_year,
                                                                 sf_month,
                                                                 sf_day)
@@ -156,7 +155,6 @@ bart_physical_0.write.save(
 bart_physical_1.write.save(
     "s3a://normalized-data-weather-bart/bart_physical_1_{}".format(
         KeyFileName), format="csv")
-
 bart_physical_2.write.save(
     "s3a://normalized-data-weather-bart/bart_physical_2_{}".format(
         KeyFileName), format="csv")
